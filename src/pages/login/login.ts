55dc184ce99640b65be4f626f876bf9f86eeb7ae
Jimi-Duo <me@feyijimi.com>
2018-12-08 21:23:04 +0100

    login, signup and reset flows complete - pre test

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
-/**
- * Generated class for the LoginPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import { IonicPage, NavController } from 'ionic-angular';
+import {
+  Alert,
+  AlertController,
+  Loading,
+  LoadingController,
+} from 'ionic-angular';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { EmailValidator } from '../../validators/email';
+import { AuthProvider } from '../../providers/auth/auth';
+import {HomePage} from '../home/home';
 
 @IonicPage()
 @Component({
@@ -15,11 +18,66 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class LoginPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+public loginForm: FormGroup;
+public loading: Loading;
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad LoginPage');
-  }
+  constructor(
+    public navCtrl: NavController, 
+    public loadingCtrl: LoadingController,
+    public alertCtrl: AlertController,
+    public authProvider: AuthProvider,
+    formBuilder: FormBuilder
+    ) {
+      this.loginForm = formBuilder.group({
+        email: [
+          '',
+          Validators.compose([Validators.required, EmailValidator.isValid])
+        ],
+        password: [
+          '',
+          Validators.compose([Validators.required, Validators.minLength(6)])
+        ]
+      });
+    }
+
+    goToSignup():void {
+      this.navCtrl.push('SignupPage');
+    }
+    
+    goToResetPassword():void {
+      this.navCtrl.push('ResetPasswordPage');
+    }
+
+    loginUser(): void {
+      if (!this.loginForm.valid) {
+        console.log(
+          `Form is not valid yet, current value: ${this.loginForm.value}`
+        );
+      } else {
+        const email = this.loginForm.value.email;
+        const password = this.loginForm.value.password;
+    
+        this.authProvider.loginUser(email, password).then(
+          authData => {
+            this.loading.dismiss().then(() => {
+              this.navCtrl.setRoot(HomePage);
+            });
+          },
+          error => {
+            this.loading.dismiss().then(() => {
+              const alert: Alert = this.alertCtrl.create({
+                message: error.message,
+                buttons: [{ text: 'Ok', role: 'cancel' }]
+              });
+              alert.present();
+            });
+          }
+        );
+        this.loading = this.loadingCtrl.create();
+        this.loading.present();
+      }
+    }
+
+
 
 }
--- src/pages/signup/signup.ts
+++ src/pages/signup/signup.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
-/**
- * Generated class for the SignupPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import {
+  Alert,
+  AlertController,
+  Loading,
+  LoadingController,
+} from "ionic-angular";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { AuthProvider } from "../../providers/auth/auth";
+import { EmailValidator } from "../../validators/email";
+import { HomePage } from "../home/home";
 
 @IonicPage()
 @Component({
@@ -14,12 +17,55 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'signup.html',
 })
 export class SignupPage {
+  public signupForm: FormGroup;
+  public loading: Loading;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(
+    public navCtrl: NavController,
+    public authProvider: AuthProvider,
+    public loadingCtrl: LoadingController,
+    public alertCtrl: AlertController,
+    formBuilder: FormBuilder
+  ) {
+    this.signupForm = formBuilder.group({
+      email: [
+        "",
+        Validators.compose([Validators.required, EmailValidator.isValid])
+      ],
+      password: [
+        "",
+        Validators.compose([Validators.minLength(6), Validators.required])
+      ]
+    });
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad SignupPage');
+  signupUser(): void {
+    if (!this.signupForm.valid) {
+      console.log(
+        `Need to complete the form, current value: ${this.signupForm.value}`
+      );
+    } else {
+      const email: string = this.signupForm.value.email;
+      const password: string = this.signupForm.value.password;
+  
+      this.authProvider.signupUser(email, password).then(
+        user => {
+          this.loading.dismiss().then(() => {
+            this.navCtrl.setRoot(HomePage);
+          });
+        },
+        error => {
+          this.loading.dismiss().then(() => {
+            const alert: Alert = this.alertCtrl.create({
+              message: error.message,
+              buttons: [{ text: "Ok", role: "cancel" }]
+            });
+            alert.present();
+          });
+        }
+      );
+      this.loading = this.loadingCtrl.create();
+      this.loading.present();
+    }
   }
-
 }
